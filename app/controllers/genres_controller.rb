43b470eadd20c377fc4f8e064055cfab221330c1
mayachacha <asakawa926@gmail.com>
2023-08-28 19:54:45 +0900

    カテゴリー階層機能

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,7 @@
 class GenresController < ApplicationController
   def show
     @genre = Genre.find(params[:id]) # 特定のジャンルをデータベースから取得
-    @dresses = @genre.dresses.order('created_at DESC') # 特定のジャンルに属するドレスをデータベースから取得し、新しいものから順に並べ替え
-      end
+    @dresses = @genre.dresses.order('created_at DESC')# 子孫カテゴリーに属するドレスをデータベースから取得し、新しいものから順に並べ替え
   end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "dresses#index"
-  resources :dresses
+  resources :dresses 
   resources :genres
+  get '/dresses/:id/genre', to: 'dresses#search'
 end
